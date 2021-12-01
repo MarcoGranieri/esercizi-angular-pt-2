46a3eb8def6c58d40479fdaf6d72e8d68c8574b4
MarcoGranieri <marcogranieri@libero.it>
2021-12-01 11:12:32 +0100

    completed exercise 9.1

---
+++ src/app/counter.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  private counter = 0;
+
+  defaultValue = 1;
+
+  constructor() { }
+
+  returnCounter () {
+    if (this.counter >= 0 ) {
+      return this.counter;
+    } else alert ('error');
+  }
+
+  addCounter () {
+    console.log(this.counter -= this.defaultValue);
+  }
+
+  subCounter () {
+     console.log(this.counter -= this.defaultValue);
+  }
+}
